de224f0c5a21e4c3efebdb789c9462f7047e1022
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-03-20 17:46:49 +0500

    created Book class

--- Code.cpp
+++ Code.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+class Book {
+    string title;
+    string author;
+    int publicationYear;
+
+public:
+    Book(string title, string author, int publicationYear) : title(title), author(author), publicationYear(publicationYear) {}
+
+    string getTitle() const {
+        return title;
+    }
+
+    string getAuthor() const {
+        return author;
+    }
+
+    int getPublicationYear() const {
+        return publicationYear;
+    }
+};
